6669554f28a100b202f0ff057b5052e697a81086
Rafa <rafa@elementai.com>
2020-07-21 20:12:17 +0100

    Run tests using config and app factory

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,11 +1,19 @@
+import os
+
 import pytest
 from starlette.testclient import TestClient
 
 from app.main import create_application
+from app.config import Settings, get_settings
+
+
+def get_settings_override():
+    return Settings(testing=1, ddatabase_url=os.environ.get("DATABASE_URL_TEST"))
 
 
 @pytest.fixture(scope="module")
 def test_app():
     app = create_application()
-    client = TestClient(app)
-    yield client
+    app.dependency_override[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+        yield test_client
