36f57733bd364b72137a63b42fce57bc39577f92
Rafa <rafa@elementai.com>
2020-07-22 09:14:15 +0100

    Get envs the original way

--- project/app/db.py
+++ project/app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import (Column, DateTime, Integer, MetaData, String, Table,
                         create_engine)
 from sqlalchemy.sql import func
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
