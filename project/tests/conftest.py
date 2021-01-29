713a1c8bb6c45baf6d6c72dfe52f82fc71ef4313
Rafa <rafa@elementai.com>
2021-01-29 21:05:45 +0000

    Try again to run tests

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,7 +3,6 @@ import os
 import pytest
 from starlette.testclient import TestClient
 
-from app.main import create_application
 from app.config import Settings, get_settings
 
 
@@ -13,8 +12,13 @@ def get_settings_override():
     )
 
 
+get_settings_override()
+
+
 @pytest.fixture(scope="module")
 def test_app():
+    from app.main import create_application
+
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
