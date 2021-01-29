bbc3a0690fb2a885562d7445ad287e888da34c05
Rafa <rafa@elementai.com>
2021-01-29 17:03:14 +0000

    Strip down container for quick testing

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,8 +3,8 @@ import os
 import pytest
 from starlette.testclient import TestClient
 
-from app.config import Settings, get_settings
 from app.main import create_application
+from app.config import Settings, get_settings
 
 
 def get_settings_override():
@@ -13,11 +13,9 @@ def get_settings_override():
     )
 
 
-app = create_application()
-app.dependency_overrides[get_settings] = get_settings_override
-
-
 @pytest.fixture(scope="module")
 def test_app():
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         yield test_client
