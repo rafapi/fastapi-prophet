013cc1974e490c9cc557440723617596d9133cc5
Rafa <rafa@elementai.com>
2021-01-29 23:31:56 +0000

    Try a new way to pass env vars

--- project/app/db.py
+++ project/app/db.py
@@ -1,10 +1,16 @@
-import os
-
+from fastapi import Depends
 from databases import Database
 from sqlalchemy import create_engine, MetaData
+from app.config import Settings, get_settings
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+async def get_env(settings: Settings = Depends(get_settings)):
+    return {
+        "database_url": settings.database_url,
+    }
+
+
+DATABASE_URL = get_env()
 
 # create database schema
 metadata = MetaData()
--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -4,18 +4,20 @@ import pytest
 from starlette.testclient import TestClient
 
 from app.main import create_application
-from app.config import Settings, get_settings
+from app import config
 
 
 def get_settings_override():
-    return Settings(
+    return config.Settings(
         testing=1, database_url=os.environ.get("DATABASE_TEST_URL")
     )
 
 
+app = create_application()
+app.dependency_overrides[config.get_settings] = get_settings_override
+
+
 @pytest.fixture(scope="module")
 def test_app():
-    app = create_application()
-    app.dependency_overrides[get_settings] = get_settings_override
     with TestClient(app) as test_client:
         yield test_client
