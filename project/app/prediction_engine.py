ed50ae5e4fc5f696529eed0f9ca03ded92006805
Rafa <rafa@elementai.com>
2021-02-03 12:02:59 +0000

    Raise error if no ticker has been trained

--- project/app/prediction_engine.py
+++ project/app/prediction_engine.py
@@ -4,9 +4,13 @@ from app.models.pydantic import StockIn
 from app.models.sqlalchemy import predictions
 from app.prophet import convert, predict
 
+from fastapi import HTTPException
+
 
 async def generate_prediction(id: int, ticker: StockIn, db):
     prediction_list = await predict(ticker)
+    if not prediction_list:
+        raise HTTPException(status_code=404, detail="No train model found")
     prediction_data = json.dumps(convert(prediction_list))
 
     query = (
