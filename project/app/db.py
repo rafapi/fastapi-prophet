d3a1fb95ac02ffff6e4620567ac82135d3a0b81e
Rafa <rafa@elementai.com>
2021-02-03 21:56:42 +0000

    Be selective about settings

--- project/app/db.py
+++ project/app/db.py
@@ -2,25 +2,19 @@ from databases import Database
 from sqlalchemy import create_engine
 
 from app.config import get_settings
-from app.models.sqlalchemy import metadata
 
 settings = get_settings()
 
 
-def mk_engine():
-    dburl = settings.database_url
-    if settings.testing:
-        engine = create_engine(dburl, connect_args={"check_same_thread": False})
-    else:
-        engine = create_engine(dburl)
-
-    metadata.create_all(engine)
+dburl = settings.database_url
+if settings.testing:
+    engine = create_engine(dburl, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(dburl)
 
 
-def setup_db() -> Database:
-    dburl = settings.database_url
-    if settings.testing:
-        database = Database(dburl, force_rollback=True)
-    else:
-        database = Database(dburl)
-    return database
+dburl = settings.database_url
+if settings.testing:
+    database = Database(dburl, force_rollback=True)
+else:
+    database = Database(dburl)
