83d1e19994e42f41c123dfc45a86345eac7a7df0
Rafa <rafa@elementai.com>
2021-02-08 11:19:30 +0000

    Use factory pattern

--- project/app/main.py
+++ project/app/main.py
@@ -3,33 +3,32 @@ import logging
 from fastapi import FastAPI
 
 from app.api import ping, predictions, train
-from app.db import engine, database
+from app.db import get_eng, get_db
 from app.models.sqlalchemy import metadata
 
 log = logging.getLogger(__name__)
 
 
 def create_application() -> FastAPI:
-    application = FastAPI()
-    application.include_router(ping.router)
-    application.include_router(
-        predictions.router, prefix="/predict", tags=["predict"]
-    )
-    application.include_router(train.router, prefix="/train", tags=["train"])
-    return application
+    app = FastAPI()
+    app.include_router(ping.router)
+    app.include_router(predictions.router, prefix="/predict", tags=["predict"])
+    app.include_router(train.router, prefix="/train", tags=["train"])
+
+    metadata.create_all(get_eng())
+    database = get_db()
+
+    @app.on_event("startup")
+    async def startup():
+        log.info("Starting up...")
+        await database.connect()
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        log.info("Shutting down...")
+        await database.disconnect()
+
+    return app
 
 
-metadata.create_all(engine)
 app = create_application()
-
-
-@app.on_event("startup")
-async def startup():
-    log.info("Starting up...")
-    await database.connect()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    log.info("Shutting down...")
-    await database.disconnect()
