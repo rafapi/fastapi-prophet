542eeddc3051b361f0b32448f7f97638c958b6b8
Rafa <rafa@elementai.com>
2021-02-08 11:16:44 +0000

    Pass db object as a parmeter

--- project/app/prediction_engine.py
+++ project/app/prediction_engine.py
@@ -1,14 +1,13 @@
 import json
 
 from app.models.pydantic import StockIn
-from app.db import database
 from app.models.sqlalchemy import predictions
 from app.prophet import convert, predict
 
 from fastapi import HTTPException
 
 
-async def generate_prediction(id: int, ticker: StockIn):
+async def generate_prediction(id: int, ticker: StockIn, database):
     prediction_list = await predict(ticker)
     if not prediction_list:
         raise HTTPException(status_code=404, detail="No train model found")
