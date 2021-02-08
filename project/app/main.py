0a33ce5285165de654b9da3214142a090d924115
Rafa <rafa@elementai.com>
2021-02-08 12:50:04 +0000

    Move up/down funs out of facory

--- project/app/main.py
+++ project/app/main.py
@@ -10,25 +10,27 @@ log = logging.getLogger(__name__)
 
 
 def create_application() -> FastAPI:
-    app = FastAPI()
-    app.include_router(ping.router)
-    app.include_router(predictions.router, prefix="/predict", tags=["predict"])
-    app.include_router(train.router, prefix="/train", tags=["train"])
-
-    metadata.create_all(get_eng())
-    database = get_db()
-
-    @app.on_event("startup")
-    async def startup():
-        log.info("Starting up...")
-        await database.connect()
-
-    @app.on_event("shutdown")
-    async def shutdown():
-        log.info("Shutting down...")
-        await database.disconnect()
-
-    return app
+    application = FastAPI()
+    application.include_router(ping.router)
+    application.include_router(
+        predictions.router, prefix="/predict", tags=["predict"]
+    )
+    application.include_router(train.router, prefix="/train", tags=["train"])
+    return application
 
 
+metadata.create_all(get_eng())
+database = get_db()
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup():
+    log.info("Starting up...")
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    log.info("Shutting down...")
+    await database.disconnect()
