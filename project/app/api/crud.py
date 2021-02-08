aa96c5eb6f0d221d08c1b141f4ab073724be3241
Rafa <rafa@elementai.com>
2021-02-08 11:16:09 +0000

    Pass db object as a parmeter

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,23 +1,22 @@
 from app.models.pydantic import StockIn
 from app.models.sqlalchemy import predictions
-from app.db import database
 
 
-async def post(payload: StockIn):
+async def post(payload: StockIn, database):
     query = predictions.insert().values(ticker=payload.ticker)
     return await database.execute(query=query)
 
 
-async def get(id: int):
+async def get(id: int, database):
     query = predictions.select().where(id == predictions.c.id)
     return await database.fetch_one(query=query)
 
 
-async def get_all():
+async def get_all(database):
     query = predictions.select()
     return await database.fetch_all(query=query)
 
 
-async def delete(id: int):
+async def delete(id: int, database):
     query = predictions.delete().where(id == predictions.c.id)
     return await database.execute(query=query)
