0ccced9a1a1e0a5f9e22bc845a0cd388703d8d31
Rafa <rafa@elementai.com>
2021-02-08 11:16:28 +0000

    Pass db object as a parmeter

--- project/app/api/predictions.py
+++ project/app/api/predictions.py
@@ -2,9 +2,10 @@ import os
 import pathlib
 from typing import List
 
-from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Path, Depends
 
 from app.api import crud
+from app.db import get_db
 from app.models.pydantic import PredictionSchema, StockIn, StockOut
 from app.prediction_engine import generate_prediction
 from app.utils import pred_to_dict
@@ -20,6 +21,7 @@ TRAINED_DIR = pathlib.Path(BASE_DIR) / "trained"
 async def create_prediction(
     payload: StockIn,
     background_tasks: BackgroundTasks,
+    database=Depends(get_db),
 ):
     model_file = TRAINED_DIR / f"{payload.ticker}.joblib"
     for entry in os.listdir(TRAINED_DIR):
@@ -27,10 +29,10 @@ async def create_prediction(
             print(entry)
     if not model_file.exists():
         raise HTTPException(status_code=404, detail="No train model found")
-    prediction_id = await crud.post(payload)
+    prediction_id = await crud.post(payload, database)
 
     background_tasks.add_task(
-        generate_prediction, prediction_id, payload.ticker
+        generate_prediction, prediction_id, payload.ticker, database
     )
 
     response_object = {"id": prediction_id, "ticker": payload.ticker}
@@ -39,8 +41,10 @@ async def create_prediction(
 
 
 @router.get("/{id}/", response_model=PredictionSchema)
-async def get_prediction(id: int = Path(..., gt=0)) -> PredictionSchema:
-    prediction_items = await crud.get(id)
+async def get_prediction(
+    id: int = Path(..., gt=0), database=Depends(get_db)
+) -> PredictionSchema:
+    prediction_items = await crud.get(id, database)
     if not prediction_items:
         raise HTTPException(status_code=404, detail="Prediction not found")
 
@@ -48,19 +52,23 @@ async def get_prediction(id: int = Path(..., gt=0)) -> PredictionSchema:
 
 
 @router.get("/", response_model=List[PredictionSchema])
-async def get_all_predictions() -> List[PredictionSchema]:
-    prediction_items = await crud.get_all()
+async def get_all_predictions(
+    database=Depends(get_db),
+) -> List[PredictionSchema]:
+    prediction_items = await crud.get_all(database)
     if not prediction_items:
         raise HTTPException(status_code=404, detail="Predictions not found")
     return [pred_to_dict(pred) for pred in prediction_items]
 
 
 @router.delete("/{id}/", response_model=PredictionSchema)
-async def delete_prediction(id: int = Path(..., gt=0)) -> PredictionSchema:
-    prediction = await crud.get(id)
+async def delete_prediction(
+    id: int = Path(..., gt=0), database=Depends(get_db)
+) -> PredictionSchema:
+    prediction = await crud.get(id, database)
     if not prediction:
         raise HTTPException(status_code=404, detail="Prediction not found")
 
-    await crud.delete(id)
+    await crud.delete(id, database)
 
     return pred_to_dict(prediction)
