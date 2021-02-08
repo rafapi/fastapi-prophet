b1b996f5fb4d2b560b9fbccd56277351504e74bb
Rafa <rafa@elementai.com>
2021-02-08 11:18:59 +0000

    Instantiate db and eng on func call

--- project/app/db.py
+++ project/app/db.py
@@ -1,18 +1,35 @@
 # import os
+from typing import Optional
 from databases import Database
 from sqlalchemy import create_engine
 
 from app.config import get_settings
 
-settings = get_settings()
+
+database = Optional[Database]
 
 
-# dburl = os.getenv("DATABASE_URL")
-dburl = settings.database_url
-# if os.getenv("TESTING"):
-if settings.testing:
-    engine = create_engine(dburl, connect_args={"check_same_thread": False})
-    database = Database(dburl, force_rollback=True)
-else:
-    engine = create_engine(dburl)
-    database = Database(dburl)
+def get_eng():
+    settings = get_settings()
+    dburl = settings.database_url
+    if settings.testing:
+        engine = create_engine(
+            dburl, connect_args={"check_same_thread": False}
+        )
+    else:
+        engine = create_engine(dburl)
+    return engine
+
+
+def get_db() -> Database:
+    global database
+    settings = get_settings()
+    dburl = settings.database_url
+    # if database.is_connected:
+    #     database = database.connection()
+    if settings.testing:
+        database = Database(dburl, force_rollback=True)
+    else:
+        database = Database(dburl)
+
+    return database
