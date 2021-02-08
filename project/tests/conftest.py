feccdf8eb4f8604864bda71b91a6943679088d30
Rafa <rafa@elementai.com>
2021-02-08 12:51:08 +0000

    Remove async and create tables for tests

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,29 +1,23 @@
 import os
 
-import asyncio
+# import asyncio
 import pytest
 from fastapi.testclient import TestClient
 
-from app.config import get_settings, Settings
-
-from app.db import database
+from app.config import get_settings
+from app.db import get_eng, get_db
 from app.main import create_application
+from app.models.sqlalchemy import metadata
 
 
 settings = get_settings()
 
 
-@pytest.fixture(scope="module")
-def event_loop():
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
-
-
-def get_settings_override():
-    return Settings(
-        testing=1, database_url=os.environ.get("DATABASE_TEST_URL")
-    )
+# @pytest.fixture(scope="module")
+# def event_loop():
+#     loop = asyncio.get_event_loop()
+#     yield loop
+#     loop.close()
 
 
 @pytest.fixture(scope="module")
@@ -37,7 +31,7 @@ def test_app():
 
 
 @pytest.fixture(scope="module")
-async def db():
-    await database.connect()
-    yield
-    await database.disconnect()
+def db():
+    metadata.create_all(get_eng())
+    database = get_db()
+    yield database
